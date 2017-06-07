8583f55918b441398f4b20c1ac8327f7f81681e8
DenisM <denism@Deniss-MacBook-Pro.local>
2017-06-07 11:53:57 +0100

    hangman.py

--- Python_for_Programmers/hangman.py
+++ Python_for_Programmers/hangman.py
@@ -11,45 +11,6 @@
 # 8. All letters found - Print 'You Won'
 # 9. Lives < 0 - Print 'You Lost'
 
-secret = list('secret'.lower())
-secret_length = len(secret)
-guesses = 10
-
-def calculate():
-
-	global guesses
-	global secret_length 
-	answer = ()
-	
-	
-
-	while guesses > 0 and secret_length > 0:
-		guess = input("There are " + str(secret_length) + " letters left in the word. Guess a letter: ")
-		if guess in secret:
-			answer = answer + (guess,)
-			print(answer)
-			secret_length -= 1
-			if secret_length == 0:
-				print('You bloody well gone and won it mate!')
-		elif guess not in secret:
-			print('Guess again')
-			guesses -= 1
-			if guesses == 0:
-				print('End of the line fella!')
-
-
-#calculate()
-
-
-
-
-
-
-
-
-
-
-
 
 #################################################
 
@@ -103,6 +64,7 @@ def askforletter():
 		print("On no! That wasn't in there!")
 		incorrectletters.append(letterguess)
 		global lives 
+		lives -= 1
 	time.sleep(2)
 
 
